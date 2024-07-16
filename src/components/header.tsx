adb7850fc4edad49920c839a056a73cd4e36969f
Mateus Ataide <teteulima@outlook.com>
2024-07-16 13:33:14 -0300

    feat/Hero: implemented framer motion in application

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,46 @@
 'use client'
 
+import { motion, useMotionValueEvent, useScroll } from 'framer-motion';
 import { useTranslations } from 'next-intl';
 import { Link } from 'next-view-transitions';
 import Image from 'next/image';
+import { useState } from 'react';
 import { MenuHeader } from './menuHeader';
 
 export default function Header() {
   const t = useTranslations('Header.links');
-  return (
-    <div className='flex justify-between items-center px-[172px] py-5 border-b-2 border-zinc-300'>
-      <div className='flex justify-center items-center gap-3'>
-        <Image src='/icon_header.png' width={12} height={12} alt='Logo'/>
-        <p className='text-lg font-bold '>Mateus Ataide</p>
 
-      </div>
+  const { scrollY} = useScroll()
+
+  const [hidden, setHidden] = useState(false)
+
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    const previous = scrollY.getPrevious() ?? 0;
+
+    if (latest > previous && latest> 150) {
+      setHidden(true)
+    } else{
+      setHidden(false)
+    }
+  })
+    
+  return (
+    <motion.div 
+    className=' sticky top-0 z-50 flex justify-between items-center px-[172px] py-5 border-b-2 border-zinc-300'
+    variants={{
+      visible: { y: 0},
+      hidden: { y: '-100%'}
+    }}
+    animate={hidden ? 'hidden' : 'visible'}
+    transition={{ duration: 0.35, ease: 'easeInOut'}}
+    >
+      <Link href='/'>
+        <div className='flex justify-center items-center gap-3'>
+          <Image src='/icon_header.png' width={12} height={12} alt='Logo'/>
+          <p className='text-lg font-bold '>Mateus Ataide</p>
+
+        </div>
+      </Link>
 
         <div className='flex justify-center items-center gap-5'>
             <Link href='/' className=' leading-5 hover:underline-after no-underline text-[14px] font-medium p-3 rounded-md'>{t('home')}</Link>
@@ -22,6 +49,6 @@ export default function Header() {
             <Link href='/' className=' leading-5 hover:underline-after no-underline text-[14px] font-medium p-3 rounded-md'>{t('curriculo')}</Link>
             <MenuHeader/>
         </div>
-    </div>
+    </motion.div>
   );
 }
