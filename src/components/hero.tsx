462016d6ae04f11d1966e8f5c8c32b7230b1fc35
Mateus Ataide <teteulima@outlook.com>
2024-07-16 13:27:34 -0300

    feat/Hero: started to create hero section

---
+++ src/components/hero.tsx
@@ -0,0 +1,19 @@
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import InputHero from "./inputHero";
+
+export default function Hero() {
+    const t = useTranslations('Home.Hero');
+    return (
+       <div className="flex justify-center px-40 py-12 gap-8">
+            <Image className="rounded-xl" src='/image_hero.png' width={440} height={390} alt='Hero'/>
+
+            <div className="flex flex-col justify-center gap-5">
+                <h1 className="text-2xl font-bold ">{t('introduction')} <span className="alternative2-gradient-text">{t('office')}</span></h1>
+                <p className="text-base font-light text-zinc-600">{t('passion')}</p>
+                <InputHero/>
+            </div>
+       </div> 
+    )
+
+}
