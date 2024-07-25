f01441df0a762ad93adcd1ba961ea7392c6f1096
Mateus Ataide <teteulima@outlook.com>
2024-07-24 22:59:50 -0300

    feat/AdjustFooter: finished footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,43 +1,71 @@
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer() {
   const t = useTranslations('Footer');
-    return (
-      <footer className="flex flex-1 justify-center border-t-2 border-lime-200 bg-FooterBg mt-9 text-LightBrown">
-        <div className="flex max-w-[960px] flex-1 flex-col">
-          <footer className="flex flex-col gap-6 px-5 py-10 text-center @container">
-            <div className="flex flex-wrap items-center justify-center gap-6 @[480px]:flex-row @[480px]:justify-around">
-              <a className=" text-base font-normal leading-normal min-w-40" href="#">{t('privacy')}</a>
-              <a className=" text-base font-normal leading-normal min-w-40" href="#">{t('terms')}</a>
-            </div>
-            <div className="flex flex-wrap justify-center gap-4">
-              <Link href="https://www.linkedin.com/in/mateus-ataide/" target="_blank">
-                <div>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
-                    <path d="M216,24H40A16,16,0,0,0,24,40V216a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V40A16,16,0,0,0,216,24Zm0,192H40V40H216V216ZM96,112v64a8,8,0,0,1-16,0V112a8,8,0,0,1,16,0Zm88,28v36a8,8,0,0,1-16,0V140a20,20,0,0,0-40,0v36a8,8,0,0,1-16,0V112a8,8,0,0,1,15.79-1.78A36,36,0,0,1,184,140ZM100,84A12,12,0,1,1,88,72,12,12,0,0,1,100,84Z"></path>
-                  </svg>
-                </div>
-              </Link>
-              <Link href="https://github.com/ata1de" target="_blank">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
-                    </svg>
-                </div>
-              </Link>
-              <Link href="https://github.com/ata1de" target="_blank">
-                <div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
-                </svg>  
-                </div>
-              </Link>
-            </div>
-            <p className="text-base font-normal leading-normal">© 2024 Mateus Ataide</p>
-          </footer>
+  const currentLocale = useLocale();
+
+  function FlagChange() {
+    if (currentLocale === 'en') {
+      return (
+        <div className='flex justify-start items-center gap-2'>
+          <Image src='/flags/estados-unidos.png' width={10} height={10} alt='Hero'/>
+          <p className="text-xs">English</p>
         </div>
-      </footer>
+      )
+    } else if (currentLocale === 'pt') {
+      return (
+        <div className='flex justify-start items-center gap-2'>
+          <Image src='/flags/brasil.png' width={10} height={10} alt='Hero'/>
+          <p className="text-xs">Português</p>
+        </div>
+      )
+    } else {
+      return (
+        <div className='flex justify-start items-center gap-2'>
+          <Image src='/flags/espanha.png' width={10} height={10} alt='Hero'/>
+          <p className="text-xs">Español</p>
+        </div>
+      )
+    }
+  }
+
+    return (
+      <div className="w-full bg-FooterBg justify-between flex items-center min-h-[300px] p-6 mt-5 text-[#F6F6F6]">
+            <div className="flex flex-col gap-8">
+              <p className="text-[#669b94] text-3xl mb-5">{t('title')}</p>
+
+              <div className="flex justify-between items-center">
+                <div className="flex flex-col gap-1 justify-start mr-5">
+                  <p className=" text-xs">{t('self')} Mateus Ataide👊🏽</p>
+                  <p className=" text-xs mb-2">{t('description')}</p>
+                  {FlagChange()}
+                </div>
+              </div>
+            </div>
+
+            <div className='flex flex-col justify-center items-center md:items-end gap-4'>
+                <div>
+                    <p className='font-bold text-center md:text-right'>{t('communications.contact')}</p>
+                    <p className=' text-center md:text-right text-sm'>98688-4201 | 98857-5153</p>
+                    <p className=' text-center md:text-right text-sm'>mateusataide05@gmail.com</p>
+                </div>
+                <div>
+                    <p className='font-bold text-center md:text-right '>{t('communications.social-media')}</p>
+                    <Link href='https://www.instagram.com/arkanseguros/' target='_blank' className='no-underline'>
+                        <p className=' text-center md:text-right text-sm'>Instagram</p>
+                    </Link>
+                    <Link href="https://wa.me/+5581986884201?text=Ol%C3%A1%2C+venha+nos+conhecer%2C+n%C3%A3o+hesite+em+mandar+mensagem%21%21" target='_blank' className='no-underline'>
+                        <p className=' text-center md:text-right text-sm'>Whatsaap</p>
+                    </Link>
+                    <Link href='https://www.facebook.com/arkanseguro/' target='_blank' className='no-underline'>
+                        <p className='text-[#F6F6F6] text-center md:text-right text-sm'>Facebook</p>
+                    </Link>    
+                </div>
+            </div>
+
+      </div>
     );
   }
   
