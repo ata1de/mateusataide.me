521637fb1acd0e54fc279bf554a56831c5c6baa3
Mateus Ataide <teteulima@outlook.com>
2024-08-02 17:07:57 -0300

    feat/PlannerProject: organized projects images in folders and added planner backend in portifolio

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -11,8 +11,8 @@ export interface ProjectType {
 export const projectsListHome: ProjectType[] = [
     {
         id: "libertas",
-        image: "/projects/mockup-libertas.png",
-        image_details: "/projects/libertas-details.svg",
+        image: "/projects/libertas/mockup-libertas.png",
+        image_details: "/projects/libertas/libertas-details.svg",
         link_visit: "https://libertas-frontend.vercel.app/",
         link_source: "https://github.com/ata1de/Libertas_frontend",
         project_video: '',
@@ -20,8 +20,8 @@ export const projectsListHome: ProjectType[] = [
     },
     {
         id: "ecommerce-store",
-        image: "/projects/mockup-ecommerce.png",
-        image_details: "/projects/ecommerce-details.png",
+        image: "/projects/ecommerce-store/mockup-ecommerce.png",
+        image_details: "/projects/ecommerce-store/ecommerce-details.png",
         link_visit: "https://ecommerce-store-vert.vercel.app/",
         link_source: "https://github.com/ata1de/Ecommerce-Store",
         project_video: '',
@@ -29,8 +29,8 @@ export const projectsListHome: ProjectType[] = [
     },
     {
         id: "dashboard-arkan-seguros",
-        image: "/projects/mockup-dashboard-arkan.png",
-        image_details: "/projects/dashboard-arkan-details.png",
+        image: "/projects/dashboard-arkan/mockup-dashboard-arkan.png",
+        image_details: "/projects/dashboard-arkan/dashboard-arkan-details.png",
         link_visit: "",
         link_source: "https://github.com/ata1de/ArkanSeguros",
         project_video: 'https://github.com/user-attachments/assets/5f03bb47-d804-45d4-baae-0f26d181489f',
@@ -39,8 +39,8 @@ export const projectsListHome: ProjectType[] = [
 
     {
         id: "arkan-seguros",
-        image: "/projects/mockup-arkan.png",
-        image_details: "/projects/arkan-details.svg",    
+        image: "/projects/arkan/mockup-arkan.png",
+        image_details: "/projects/arkan/arkan-details.svg",    
         link_visit: "",
         link_source: "https://github.com/ata1de/ArkanSeguros",
         project_video: 'https://github.com/user-attachments/assets/0e2177fa-c0ed-4ce9-b9ad-f9e014c0c5e8',
@@ -49,8 +49,8 @@ export const projectsListHome: ProjectType[] = [
 
     {
         id: "onebitflix-front",
-        image: "/projects/mockup-onebitflix.png",
-        image_details: "/projects/onebitflix-details.png",
+        image: "/projects/onebitflix-front/mockup-onebitflix.png",
+        image_details: "/projects/onebitflix-front/onebitflix-details.png",
         link_visit: "",
         link_source: "https://github.com/ata1de/onebitflix_frontend",
         project_video: 'https://github.com/user-attachments/assets/f7df90c5-a8f4-4d20-bdef-6615c3afd56b',
@@ -58,8 +58,8 @@ export const projectsListHome: ProjectType[] = [
     },
     {
         id: "onebitflix-back",
-        image: "/projects/mockup-onebitflix-back.png",
-        image_details: "/projects/onebitflix-backend-details.png",
+        image: "/projects/onebitflix-back/mockup-onebitflix-back.png",
+        image_details: "/projects/onebitflix-back/onebitflix-backend-details.png",
         link_visit: "",
         link_source: "https://github.com/ata1de/onebitflix_backend",
         project_video: '',
@@ -67,8 +67,8 @@ export const projectsListHome: ProjectType[] = [
     },
     {
         id: "python-analytics",
-        image: "/projects/mockup-python-analytics.png",
-        image_details: "/projects/python-analytics-details.png",
+        image: "/projects/python-analytics/mockup-python-analytics.png",
+        image_details: "/projects/python-analytics/python-analytics-details.png",
         link_visit: "",
         link_source: "https://github.com/ata1de/PowerBi",
         project_video: '',
@@ -76,12 +76,21 @@ export const projectsListHome: ProjectType[] = [
     },
     {
         id: "libertas-backend",
-        image: "/projects/mockup-libertas-backend.png",
-        image_details: "/projects/libertas-backend-details.svg",
+        image: "/projects/libertas-back/mockup-libertas-backend.png",
+        image_details: "/projects/libertas-back/libertas-backend-details.svg",
         link_visit: "https://libertas-frontend.vercel.app/",
         link_source: "https://github.com/ata1de/libertas_backend",
         project_video: '',
         category: "back-end",
     },
+    {
+        id: "planner-back",
+        image: "/projects/planner-back/mockup-planner-back.png",
+        image_details: "/projects/planner-back/planner-back-details.svg",
+        link_visit: "",
+        link_source: "https://github.com/ata1de/backend_trip",
+        project_video: '',
+        category: "back-end",
+    }
 
 ];
